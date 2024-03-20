32a02096d4bb145f6415e5b7609530d8107eb421
Kitui Kelly <kituikelly@gmail.com>
2024-03-20 21:34:15 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file!
+// Code your solution in t
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = "Chelsea";
+
+
+
+let companyCeo = "Susan Smith";
+
+companyCeo = "Susan Smith";
